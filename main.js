c0bba1c1716e2d25ed9dc53d5ec78a3f4098b710
AnnGolovina <golovina.vit@gmail.com>
2023-05-11 13:31:51 +0300

    change cover and song name

---
+++ main.js
@@ -0,0 +1,53 @@
+const player = document.querySelector(".player");
+const playButton = document.querySelector(".play");
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+const audio = document.querySelector("audio");
+const progressWrapper = document.querySelector(".progress-wrapper");
+const progress = document.querySelector(".progress");
+const imgSrc = document.querySelector(".img-play");
+const cover = document.querySelector(".cover_img");
+const songName = document.querySelector(".song");
+
+const songs = ["comical.wav", "feeling happy.wav", "pop.wav"];
+let currentTrackIndex = 0;
+
+audio.src = songs[currentTrackIndex];
+
+playButton.onclick = (e) => {
+  audio.paused ? audio.play() : audio.pause();
+  e.target.src = audio.paused ? `img/play.png` : "img/pause.png";
+};
+
+prevButton.onclick = () => {
+  currentTrackIndex--;
+
+  if (songs[currentTrackIndex]) {
+    audio.src = songs[currentTrackIndex];
+  } else {
+    audio.src = songs.at(-1);
+    currentTrackIndex = songs.length - 1;
+  }
+  audio.play();
+  loadSong(songs[currentTrackIndex]);
+};
+
+nextButton.onclick = () => {
+  currentTrackIndex++;
+
+  if (songs[currentTrackIndex]) {
+    audio.src = songs[currentTrackIndex];
+  } else {
+    audio.src = songs[0];
+    currentTrackIndex = 0;
+  }
+  audio.play();
+  loadSong(songs[currentTrackIndex]);
+};
+
+function loadSong(song) {
+  songName.innerHTML = song;
+  cover.src = `img/img-${currentTrackIndex + 1}.jpg`;
+}
+
+//console.log(audio, "ELEMNT");
